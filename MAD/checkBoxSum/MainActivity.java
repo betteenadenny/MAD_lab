bf972bd60acea0a913bb8d22fd140d689280987a
betteenadenny <116335920+betteenadenny@users.noreply.github.com>
2023-12-18 10:50:45 +0530

    MainActivity.java

--- MAD/checkBoxSum/MainActivity.java
+++ MAD/checkBoxSum/MainActivity.java
@@ -1,12 +1,11 @@
-package com.example.myapplication;
+package com.example.checkboxsum;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,25 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CheckBox checkbox1 = findViewById(R.id.checkbox1);
-        final CheckBox checkbox2 = findViewById(R.id.checkbox2);
-        final CheckBox checkbox3 = findViewById(R.id.checkbox3);
-        final CheckBox checkbox4 = findViewById(R.id.checkbox4);
-        final CheckBox checkbox5 = findViewById(R.id.checkbox5);
-        final CheckBox checkbox6 = findViewById(R.id.checkbox6);
-        final CheckBox checkbox7 = findViewById(R.id.checkbox7);
-        final CheckBox checkbox8 = findViewById(R.id.checkbox8);
-        final CheckBox checkbox9 = findViewById(R.id.checkbox9);
+        CheckBox checkbox1 = findViewById(R.id.checkbox1);
+        CheckBox checkbox2 = findViewById(R.id.checkbox2);
+        CheckBox checkbox3 = findViewById(R.id.checkbox3);
+        CheckBox checkbox4 = findViewById(R.id.checkbox4);
+        CheckBox checkbox5 = findViewById(R.id.checkbox5);
+        CheckBox checkbox6 = findViewById(R.id.checkbox6);
+        CheckBox checkbox7 = findViewById(R.id.checkbox7);
+        CheckBox checkbox8 = findViewById(R.id.checkbox8);
+        CheckBox checkbox9 = findViewById(R.id.checkbox9);
 
-        Button calculateButton = findViewById(R.id.calculateButton);
-        calculateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Button button = findViewById(R.id.b1);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
                 int sum = 0;
-
-                // Check each checkbox and add its value to the sum if checked
-                if (checkbox1.isChecked()) {
-                    sum += 1;
+                if(checkbox1.isChecked()){
+                    sum +=1;
                 }
                 if (checkbox2.isChecked()) {
                     sum += 2;
@@ -59,13 +56,12 @@ public class MainActivity extends AppCompatActivity {
                 if (checkbox9.isChecked()) {
                     sum += 9;
                 }
-                // Display the sum in a toast message
-                showToast("Sum: " + sum);
+                showToast("Sum: "+sum);
             }
         });
     }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    public void showToast(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 }
